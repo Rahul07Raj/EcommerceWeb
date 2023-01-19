3bab1a34463e15f576e2d855a0743d862b995e86
Rahul <rahulrajputgsits@gmail.com>
2023-01-20 01:07:36 +0530

    rahul3

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+     <header>
+      <a href="/">amazon</a>
+     </header>
+     <main>
+      list products
+     </main>
+    </div>
+  );
+}
+
+export default App;
